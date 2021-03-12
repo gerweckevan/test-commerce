2068001c77a8f9ced627419444514817fa00e8fc
Evan Gerweck <gerweckevan@gmail.com>
2021-03-11 21:27:21 -0700

    main

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  secret: "admin",
+  DB_URI_DEV: "mongodb://localhost:27017/myFirstDatabase",
+  DB_URI:
+    "mongodb+srv://root:root@cluster0.nfkwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
