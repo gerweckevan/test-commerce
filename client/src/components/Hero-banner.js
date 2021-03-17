1136fb4d50c90602d60f90444f5b8384ee712f86
SterlingOrth <71182922+SterlingOrth@users.noreply.github.com>
2021-03-16 21:48:07 -0600

    updated homepage buttons, first commit

--- client/src/components/Hero-banner.js
+++ client/src/components/Hero-banner.js
@@ -1,48 +1,55 @@
-import React from 'react'
-import {
-  isMobile
-} from "react-device-detect";
-import ButtonLinkGenderPage from './Button-link-gender-page'
-import { Jumbotron, Container } from 'reactstrap';
+import React from "react";
+import { isMobile } from "react-device-detect";
+import ButtonLinkGenderPage from "./Button-link-gender-page";
+import { Jumbotron, Container } from "reactstrap";
 
 const styles = {
   bannerCoverPc: {
     backgroundImage: 'url("/images/banner-cover-pc.gif")',
-    backgroundSize: 'cover'
+    backgroundSize: "cover",
   },
   bannerCoverMobile: {
     backgroundImage: 'url("/images/banner-cover-mobile.gif")',
-    backgroundSize: 'cover'
+    backgroundSize: "cover",
   },
   textBanner: {
     textShadow: "3px 3px 3px grey",
-    textAlign: 'center',
-    color: 'white'
+    textAlign: "center",
+    color: "white",
   },
   centerButtons: {
-    textAlign: 'center',
-    padding: '30px'
+    textAlign: "center",
+    padding: "30px",
   },
   titleH1Pc: {
-    fontSize: '80px'
+    fontSize: "80px",
   },
   titleH1Mobile: {
-    fontSize: '60px'
-  }
+    fontSize: "60px",
+  },
 };
 
-const { bannerCoverPc, bannerCoverMobile, textBanner, centerButtons, titleH1Mobile, titleH1Pc } = styles
+const {
+  bannerCoverPc,
+  bannerCoverMobile,
+  textBanner,
+  centerButtons,
+  titleH1Mobile,
+  titleH1Pc,
+} = styles;
 
 const HeroBanner = () => (
   <Jumbotron fluid style={isMobile ? bannerCoverMobile : bannerCoverPc}>
-    <Container fluid style={{ height: '300px' }}>
+    <Container fluid style={{ height: "300px" }}>
       <div style={textBanner}>
-        <h1 className="display-3" style={isMobile ? titleH1Mobile : titleH1Pc}>Fashion shop</h1>
+        <h1 className="display-3" style={isMobile ? titleH1Mobile : titleH1Pc}>
+          Fashion shop
+        </h1>
         <p>Cool shop</p>
       </div>
       <div style={centerButtons}>
-        <ButtonLinkGenderPage gender={'men'} content='shop Men' />
-        <ButtonLinkGenderPage gender={'women'} content='shop Women' />
+        <ButtonLinkGenderPage gender={"men"} content="Artwork" />
+        <ButtonLinkGenderPage gender={"women"} content="Merchandise" />
       </div>
     </Container>
   </Jumbotron>
